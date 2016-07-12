d304ff4567518a4716d6ec30f29f979496e29163
Youssouf da Silva <youssoufdasilva@gmail.com>
2016-07-12 16:35:22 +0000

    Working on the recycler view

---
+++ Gavel-Project-2/app/src/main/java/com/psyphertxt/gavel/FeedItem.java
@@ -0,0 +1,74 @@
+package com.psyphertxt.gavel;
+
+/**
+ * Created by youssoufdasilva on 7/12/16.
+ */
+public class FeedItem {
+
+    private String mTitle;
+    private String mText;
+    private String mFeedType;
+    private Boolean mSeen;
+    private String mPhotoUrl;
+
+    public FeedItem(){
+
+    }
+
+    public FeedItem(String title, String text, String type, String photoUrl) {
+        mTitle = title;
+        mText = text;
+        mFeedType = type;
+        mSeen = false;
+        mPhotoUrl = photoUrl;
+    }
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String mText) {
+        this.mText = mText;
+    }
+
+    public String getFeedType() {
+        return mFeedType;
+    }
+
+    public void setFeedType(String mFeedType) {
+        this.mFeedType = mFeedType;
+    }
+
+    public Boolean getSeen() {
+        return mSeen;
+    }
+
+    public void setSeen(Boolean mSeen) {
+        this.mSeen = mSeen;
+    }
+
+    public String getPhotoUrl() {
+        return mPhotoUrl;
+    }
+
+    public void setPhotoUrl(String mPhotoUrl) {
+        this.mPhotoUrl = mPhotoUrl;
+    }
+
+    public Boolean isSeen(){
+        return getSeen();
+    }
+
+    public void makeSeen(){
+        setSeen(true);
+    }
+}
--- Gavel-Project-2/app/src/main/java/com/psyphertxt/gavel/MainFeedActivity.java
+++ Gavel-Project-2/app/src/main/java/com/psyphertxt/gavel/MainFeedActivity.java
@@ -2,22 +2,51 @@ package com.psyphertxt.gavel;
 
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.FirebaseDatabase;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
 import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainFeedActivity extends AppCompatActivity {
 
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        public TextView messageTextView;
+        public TextView messengerTextView;
+        public CircleImageView messengerImageView;
+        public ImageView messageType;
 
+        public MessageViewHolder(View v) {
+            super(v);
+            messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
+            messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
+            messengerImageView = (CircleImageView) itemView.findViewById(R.id.messengerImageView);
+            messageType = (ImageView) itemView.findViewById(R.id.messageTypeIcon);
+        }
+    }
+
+    private static final String TAG = "MainFeedActivity";
+
+    private RecyclerView mMessageRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+//TODO: change the colors of the fab buttons
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,42 +55,62 @@ public class MainFeedActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        int redActionButtonSize = getResources().getDimensionPixelSize(R.dimen.red_action_button_size);
+        int redActionButtonMargin = getResources().getDimensionPixelOffset(R.dimen.action_button_margin);
+        int redActionButtonContentSize = getResources().getDimensionPixelSize(R.dimen.red_action_button_content_size);
+        int redActionButtonContentMargin = getResources().getDimensionPixelSize(R.dimen.red_action_button_content_margin);
+        int redActionMenuRadius = getResources().getDimensionPixelSize(R.dimen.red_action_menu_radius);
+        int blueSubActionButtonSize = getResources().getDimensionPixelSize(R.dimen.blue_sub_action_button_size);
+        int blueSubActionButtonContentMargin = getResources().getDimensionPixelSize(R.dimen.blue_sub_action_button_content_margin);
 
         final ImageView iconAdd = new ImageView(this);
         iconAdd.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_white_24dp));
-//        iconAdd.setImageResource(R.drawable.ic_add_white_24dp);
+
+
+        FloatingActionButton.LayoutParams addParams = new FloatingActionButton.LayoutParams(redActionButtonSize, redActionButtonSize);
+        addParams.setMargins(redActionButtonMargin,
+                redActionButtonMargin,
+                redActionButtonMargin,
+                redActionButtonMargin);
+        iconAdd.setLayoutParams(addParams);
+
+        FloatingActionButton.LayoutParams fabIconAddParams = new FloatingActionButton.LayoutParams(redActionButtonContentSize, redActionButtonContentSize);
+        fabIconAddParams.setMargins(redActionButtonContentMargin,
+                redActionButtonContentMargin,
+                redActionButtonContentMargin,
+                redActionButtonContentMargin);
 
         FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
-                .setContentView(iconAdd)
+                .setContentView(iconAdd, fabIconAddParams)
                 .setBackgroundDrawable(R.drawable.button_action_red_selector)
+                .setLayoutParams(addParams)
                 .build();
+
+        // Set up customized SubActionButtons for the right center menu
+        SubActionButton.Builder subBuilder = new SubActionButton.Builder(this);
+        subBuilder.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_action_blue_selector));
+
+        FrameLayout.LayoutParams blueContentParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        blueContentParams.setMargins(blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin,
+                blueSubActionButtonContentMargin);
+        subBuilder.setLayoutParams(blueContentParams);
+
+        // Set custom layout params
+        FrameLayout.LayoutParams blueParams = new FrameLayout.LayoutParams(blueSubActionButtonSize, blueSubActionButtonSize);
+        subBuilder.setLayoutParams(blueParams);
 
         ImageView iconGavel = new ImageView(this);
-        iconGavel.setImageDrawable(getResources().getDrawable(R.drawable.ic_gavel_white_24dp));
-//        iconGavel.setImageResource(R.drawable.ic_gavel_white_24dp);
-
         ImageView iconChat = new ImageView(this);
+
+        iconGavel.setImageDrawable(getResources().getDrawable(R.drawable.ic_gavel_white_24dp));
         iconChat.setImageDrawable(getResources().getDrawable(R.drawable.ic_chat_white_24dp));
-//        iconChat.setImageResource(R.drawable.ic_chat_white_24dp);
 
-        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
-        itemBuilder.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_action_red_selector));
-
-        SubActionButton buttonGavel = itemBuilder.setContentView(iconGavel)
-                .build();
-        SubActionButton buttonChat = itemBuilder.setContentView(iconChat).build();
-
-        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
-                .addSubActionView(buttonGavel)
-                .addSubActionView(buttonChat)
+        final FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(subBuilder.setContentView(iconGavel, blueContentParams).build())
+                .addSubActionView(subBuilder.setContentView(iconChat, blueContentParams).build())
+                .setRadius(redActionMenuRadius)
                 .attachTo(actionButton)
                 .build();
 
@@ -86,6 +135,62 @@ public class MainFeedActivity extends AppCompatActivity {
             }
         });
 
+        // Initialize ProgressBar and RecyclerView and FirebaseAnalytics
+//        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mMessageRecyclerView = (RecyclerView) findViewById(R.id.feedRecyclerView);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager.setStackFromEnd(false); //start from the top
+        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // New child entries
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<FriendlyMessage,
+                MessageViewHolder>(
+                FriendlyMessage.class,
+                R.layout.item_message,
+                MessageViewHolder.class,
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD)) {
+
+            @Override
+            protected void populateViewHolder(MessageViewHolder viewHolder,
+                                              FriendlyMessage friendlyMessage, int position) {
+                mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+                viewHolder.messageTextView.setText(friendlyMessage.getText());
+                viewHolder.messengerTextView.setText(friendlyMessage.getName());
+                if (friendlyMessage.getPhotoUrl() == null) {
+                    viewHolder.messengerImageView
+                            .setImageDrawable(ContextCompat
+                                    .getDrawable(MainActivity.this,
+                                            R.drawable.ic_account_circle_black_36dp));
+                } else {
+                    Glide.with(MainActivity.this)
+                            .load(friendlyMessage.getPhotoUrl())
+                            .into(viewHolder.messengerImageView);
+                }
+            }
+        };
+
+        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                int friendlyMessageCount = mFirebaseAdapter.getItemCount();
+                int lastVisiblePosition =
+                        mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
+                // If the recycler view is initially being loaded or the
+                // user is at the bottom of the list, scroll to the bottom
+                // of the list to show the newly added message.
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (friendlyMessageCount - 1) &&
+                                lastVisiblePosition == (positionStart - 1))) {
+                    mMessageRecyclerView.scrollToPosition(positionStart);
+                }
+            }
+        });
+
+        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
     }
 
 }
